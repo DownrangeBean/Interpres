df9faec25f640e44560560f542e16843a8d6d02f
James Henry Stammers <james.h.stammers@gmail.com>
2019-05-03 13:58:30 +0200

    Initial commit

--- RunAllTests.py
+++ RunAllTests.py
@@ -4,11 +4,37 @@ import os
 import sys
 import shutil
 
+# TODO: onerror function for rmtree.
+#  def take_ownership()
+
+
 test_directory = Definitions.TEST_OUT_DIR
-if not os.path.isdir(test_directory):
-    os.mkdir(test_directory, 0o777)
+# TODO: check if directory exists
+if os.path.isdir(test_directory):
+    # TODO: exists: delete contents
+    with os.scandir(test_directory) as entries:
+        for entry in entries:
+            if os.path.isdir(entry):
+                try:
+                    shutil.rmtree(entry)
+                except OSError:
+                    # TODO change permisions of directory
+                    for root, dirs, files in os.walk(entry):
+                        for d in dirs:
+                            os.chmod(os.path.join(root, d), 0o777)
+                        for f in files:
+                            os.chmod(os.path.join(root, f), 0o777)
+                    # Try again
+                    shutil.rmtree(entry)
+            try:
+                os.remove(entry)
+            except:
+            # TODO: handle exception regarding a file being used.
 else:
-    shutil.rmtree(test_directory)
+    # TODO: Doesnt exists: create contents
+    os.mkdir(test_directory, 0o777)
+
+
 
 loader = unittest.defaultTestLoader
 suite = loader.discover(Definitions.ROOT_DIR)
